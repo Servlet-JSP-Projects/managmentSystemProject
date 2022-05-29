7ba047fb93624ee79af9435c848f5a34d975ca79
sara-19992 <sarasawafta999@gmail.com>
2022-05-29 12:26:46 +0300

    updated

--- src/com/managementsystem/web/EmployeeServlet.java
+++ src/com/managementsystem/web/EmployeeServlet.java
@@ -6,115 +6,122 @@ import java.util.Random;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.managementsystem.dao.EmployeeDao;
-import com.managementsystem.dao.TaskDao;
 import com.managementsystem.model.Employee;
-import com.managementsystem.model.Task;
 import com.managementsystem.model.Team;
 
 /**
  * Servlet implementation class EmployeeServlet
  */
-//@WebServlet("/Employee")
+// @WebServlet("/Employee")
 public class EmployeeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public EmployeeServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	    System.out.print("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
-
-		HttpSession session=request.getSession(false);  
-        Employee employee =(Employee) session.getAttribute("currentEmployee"); 
-        String action = request.getParameter("action");
-        String id =request.getParameter("id");
-        
-		try{
-        	switch (action) {
-        	case "all":
-        		listAllEmployee(request, response);
-  				break;
-        	case "allDeveloper":
-        		listAllDeveloper(request, response);
-  				break;
-  			case "developerTeam":
-  				if(id!=null && id!="")listDeveloperTeam(request, response,Integer.valueOf(id),employee.getEmployeeRule());
-  				else listDeveloperTeam(request, response,employee.getEmployeeID(),employee.getEmployeeRule());
-  				break;
-  			case "team":
-  				listTeam(request, response);
-  				break;
-  			case "new":
-  				showNewEmployeeForm(request, response,Integer.valueOf(id));
-  				break;
-  			case "newTeam":
-  				showNewTeamForm(request, response);
-  				break;
-  			case "insert":
-  				insertDeveloper(request, response, employee.getEmployeeRule());
-  				break;
-  			case "insertTeam":
-  				insertTeam(request, response, employee.getEmployeeRule());
-  				break;
-  			case "delete":
-  				delete(request, response,Integer.valueOf(id));
-  				break;
-  			case "logout":
-  				logout(request, response);
-  				break;
-//  			case "teamLeader":
-//  				listTask(request, response,employee.getEmployeeID());
-//  				break;
-  			}
-        	
-		} catch(Exception ex) {
-			throw new ServletException(ex);
-	    }
-			
+	public EmployeeServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.print("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+
+		HttpSession session = request.getSession(false);
+		Employee employee = (Employee) session.getAttribute("currentEmployee");
+		String action = request.getParameter("action");
+		String id = request.getParameter("id");
+
+		try {
+			switch (action) {
+			case "all":
+				listAllEmployee(request, response);
+				break;
+			case "allDeveloper":
+				listAllDeveloper(request, response);
+				break;
+			case "developerTeam":
+				if (id != null && id != "")
+					listDeveloperTeam(request, response, Integer.valueOf(id), employee.getEmployeeRule());
+				else
+					listDeveloperTeam(request, response, employee.getEmployeeID(), employee.getEmployeeRule());
+				break;
+			case "team":
+				listTeam(request, response);
+				break;
+			case "new":
+				showNewEmployeeForm(request, response, Integer.valueOf(id));
+				break;
+			case "newTeam":
+				showNewTeamForm(request, response);
+				break;
+			case "insert":
+				insertDeveloper(request, response, employee.getEmployeeRule());
+				break;
+			case "insertTeam":
+				insertTeam(request, response, employee.getEmployeeRule());
+				break;
+			case "delete":
+				delete(request, response, Integer.valueOf(id));
+				break;
+			case "logout":
+				logout(request, response);
+				break;
+			// case "teamLeader":
+			// listTask(request, response,employee.getEmployeeID());
+			// break;
+			}
+
+		} catch (Exception ex) {
+			throw new ServletException(ex);
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-	
-	private void listAllEmployee(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+
+	private void listAllEmployee(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		List<Employee> listEmployee = EmployeeDao.AllEmployee();
 		request.setAttribute("listEmployee", listEmployee);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("employee/allEmployee.jsp");
 		dispatcher.forward(request, response);
 	}
-	
-	
-	private void listAllDeveloper(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+
+	private void listAllDeveloper(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		List<Employee> listDeveloper = EmployeeDao.AllDevloper();
 		request.setAttribute("listDeveloper", listDeveloper);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("employee/allDeveloper.jsp");
 		dispatcher.forward(request, response);
 	}
-	
-	private void listDeveloperTeam(HttpServletRequest request, HttpServletResponse response, int employeeID ,String employeeRule)throws ServletException, IOException {
+
+	private void listDeveloperTeam(HttpServletRequest request, HttpServletResponse response, int employeeID,
+			String employeeRule) throws ServletException, IOException {
 		System.out.println(employeeID);
-		List<Employee> listDeveloperTeam = EmployeeDao.TeamDevloper(employeeID);	
+		List<Employee> listDeveloperTeam = EmployeeDao.TeamDevloper(employeeID);
 		request.setAttribute("listDeveloperTeam", listDeveloperTeam);
 		request.setAttribute("employeeRule", employeeRule);
 		request.setAttribute("id", employeeID);
@@ -123,64 +130,71 @@ public class EmployeeServlet extends HttpServlet {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("employee/developer.jsp");
 		dispatcher.forward(request, response);
 	}
-	
-	private void listTeam(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+
+	private void listTeam(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		List<Team> listTeam = EmployeeDao.Team();
 		request.setAttribute("listTeam", listTeam);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("employee/team.jsp");
 		dispatcher.forward(request, response);
 	}
-	
-	private void showNewEmployeeForm(HttpServletRequest request, HttpServletResponse response, int id)throws ServletException, IOException {
+
+	private void showNewEmployeeForm(HttpServletRequest request, HttpServletResponse response, int id)
+			throws ServletException, IOException {
 		request.setAttribute("id", id);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("employee/newDeveloper.jsp");
 		dispatcher.forward(request, response);
 	}
-	private void showNewTeamForm(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+
+	private void showNewTeamForm(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("employee/newTeam.jsp");
 		dispatcher.forward(request, response);
 	}
-	
-	private void insertDeveloper(HttpServletRequest request, HttpServletResponse response, String employeeRule)throws ServletException, IOException {
+
+	private void insertDeveloper(HttpServletRequest request, HttpServletResponse response, String employeeRule)
+			throws ServletException, IOException {
 		Random random = new Random();
-		int randomNum =  random.nextInt(50000000);
-		int phone= Integer.valueOf(request.getParameter("phone"));
-		int teamID=Integer.valueOf(request.getParameter("id"));
-	     Employee employee=new Employee(request.getParameter("firstName"),request.getParameter("lastName"),phone,
-	    		 request.getParameter("address"),randomNum,"Developer",request.getParameter("email"),
-	    		 "Developer");
-	     
-	     EmployeeDao.addEmployee(employee);
-	     
-	     EmployeeDao.addEmployeeTeam(teamID,randomNum);
-	     
-	     listDeveloperTeam(request,response,teamID,employeeRule);
+		int randomNum = random.nextInt(50000000);
+		String password = Integer.toString(random.nextInt(50000000));
+		int phone = Integer.valueOf(request.getParameter("phone"));
+		int teamID = Integer.valueOf(request.getParameter("id"));
+		Employee employee = new Employee(request.getParameter("firstName"), request.getParameter("lastName"), phone,
+				request.getParameter("address"), randomNum, "Developer", request.getParameter("email"), password);
+
+		EmployeeDao.addEmployee(employee);
+
+		EmployeeDao.addEmployeeTeam(teamID, randomNum);
+
+		listDeveloperTeam(request, response, teamID, employeeRule);
 	}
-	
-	private void insertTeam(HttpServletRequest request, HttpServletResponse response, String employeeRule)throws ServletException, IOException {
+
+	private void insertTeam(HttpServletRequest request, HttpServletResponse response, String employeeRule)
+			throws ServletException, IOException {
 		Random random = new Random();
-		int randomNum =  random.nextInt(50000000);
-		int phone= Integer.valueOf(request.getParameter("phone"));
-	     Employee employee=new Employee(request.getParameter("firstName"),request.getParameter("lastName"),phone,
-	    		 request.getParameter("address"),randomNum,"TeamLeader",request.getParameter("email"),
-	    		 "TeamLeader");
-	     
-	     EmployeeDao.addEmployee(employee);
-	     
-	     EmployeeDao.addTeam(randomNum,request.getParameter("teamName"));
-	     
-	     listTeam(request, response);
+		int randomNum = random.nextInt(50000000);
+		String password = Integer.toString(random.nextInt(50000000));
+		int phone = Integer.valueOf(request.getParameter("phone"));
+		Employee employee = new Employee(request.getParameter("firstName"), request.getParameter("lastName"), phone,
+				request.getParameter("address"), randomNum, "TeamLeader", request.getParameter("email"), password);
+
+		EmployeeDao.addEmployee(employee);
+
+		EmployeeDao.addTeam(randomNum, request.getParameter("teamName"));
+
+		listTeam(request, response);
 	}
-	
-	private void delete(HttpServletRequest request, HttpServletResponse response,int employeeID)throws ServletException, IOException {
+
+	private void delete(HttpServletRequest request, HttpServletResponse response, int employeeID)
+			throws ServletException, IOException {
 		EmployeeDao.deleteEmplyee(employeeID);
-		listAllEmployee(request,response);
+		listAllEmployee(request, response);
 	}
-	
-	private void logout(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
-		 HttpSession session=request.getSession(false);  
-		 session.invalidate();
-	     response.sendRedirect("home.jsp");	
+
+	private void logout(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
+		session.invalidate();
+		response.sendRedirect("home.jsp");
 	}
 
 }
--- src/com/managementsystem/web/LoginServlet.java
+++ src/com/managementsystem/web/LoginServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,48 +15,53 @@ import com.managementsystem.model.Login;
 //@WebServlet("/Login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		 response.sendRedirect("login.jsp");
-		 }
+		response.sendRedirect("login.jsp");
+	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		authenticate(request,response);
+		authenticate(request, response);
 	}
-	
-	private void authenticate(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		
+
+	private void authenticate(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
 		Login login = new Login();
 		login.setEmail(request.getParameter("email"));
 		login.setPassword(request.getParameter("password"));
 
 		try {
-			if (LoginDao.validate(login)) { 
-		          System.out.println("SARAAAAAAAAAAA");
+			if (LoginDao.validate(login)) {
+				System.out.println("SARAAAAAAAAAAA");
 
-		          HttpSession session = request.getSession(true);	    
-		          session.setAttribute("currentEmployee",LoginDao.getEmployee()); 
-		          session.setAttribute("employeeRule", LoginDao.getEmployee().getEmployeeRule());
-		          System.out.println("ccccccccccccccccc");
-//		          response.sendRedirect("task/list.jsp");
-		          RequestDispatcher dispatcher = request.getRequestDispatcher("Task?action=list");
-				  dispatcher.forward(request, response);
-//		          RequestDispatcher dispatcher = request.getRequestDispatcher("Task");
-//		  		  dispatcher.forward(request, response);
+				HttpSession session = request.getSession(true);
+				session.setAttribute("currentEmployee", LoginDao.getEmployee());
+				session.setAttribute("employeeRule", LoginDao.getEmployee().getEmployeeRule());
+				System.out.println("ccccccccccccccccc");
+				// response.sendRedirect("task/list.jsp");
+				RequestDispatcher dispatcher = request.getRequestDispatcher("Task?action=list");
+				dispatcher.forward(request, response);
+				// RequestDispatcher dispatcher =
+				// request.getRequestDispatcher("Task");
+				// dispatcher.forward(request, response);
 			} else {
-				  response.sendRedirect("login.jsp");
+				response.sendRedirect("login.jsp");
 
 			}
 		} catch (Exception e) {
